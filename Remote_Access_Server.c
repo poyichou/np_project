da5d0778eb1c7ce3d9689f98ffba842bd694e1d3
gammmmmble <bob2006tw@gmail.com>
2017-10-17 16:08:55 +0800

    server: add server prototype

--- Remote_Access_Server.c
+++ Remote_Access_Server.c
@@ -1,11 +1,59 @@
+#define MAXLINE	512
+#define SERV_TCP_PORT 5051
 #include<stdio.h>
-#include <arpa/inet.h>
+#include<string.h>
+#include<stdlib.h>
+#include<arpa/inet.h>
 void mysetenv(char* ,char* , int);
 void printenv();
+void err_dump(char*);
+void process_request(int);
+
 int main(int argc, char* argv[])
 {
+	int sockfd, newsockfd, clilen, childpid;
+	struct sockaddr_in	cli_addr, serv_addr;
+	/*
+	 *Open a TCP socket(an Internet stream socket).
+	 */
+	if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0){
+		err_dump("server:can't open stream socket");
+	}
+	//memset((char*)&serv_addr, 0, sizeof(serv_addr));
+	bzero((char *) &serv_addr, sizeof(serv_addr));
+	serv_addr.sin_family		= AF_INET;
+	serv_addr.sin_addr.s_addr	= htonl(INADDR_ANY);
+	serv_addr.sin_port			= htons(SERV_TCP_PORT);
 
+	if(bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0){
+		err_dump("server:can't bind local address");
+	}
+	listen(sockfd, 5);
+	while(1)
+	{
+		clilen = sizeof(cli_addr);
+		newsockfd = accept(sockfd, (struct sockaddr*)&cli_addr, &clilen);
+		if(newsockfd < 0){
+			err_dump("server:accept error");
+		}
+		if((childpid = fork()) < 0){
+			err_dump("server: fork error");
+		}else if(childpid == 0){/*child process*/
+			/*close original socket*/
+			close(sockfd);
+			/*process the request*/
+			//TODO process request
+			process_request(newsockfd);
+			exit(0);
+		}
+		close(newsockfd);/*parent process*/
+	}
 }
+void err_dump(char* msg){
+	perror(msg);
+	exit(1);
+}
+void process_request(int newsockfd){}
 void mysetenv(char* name, char* value, int replace){
 	if(setenv(name, value, replace) == 0){
 		printf("change env succeeded!!\n");
