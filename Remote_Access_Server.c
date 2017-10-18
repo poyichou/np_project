bb99c76e95113eb46e4fefb00422100aa4b4b221
gammmmmble <bob2006tw@gmail.com>
2017-10-18 17:21:15 +0800

    server: add parser and chekargv and discard readline temporary

--- Remote_Access_Server.c
+++ Remote_Access_Server.c
@@ -1,4 +1,5 @@
-#define MAXLINE	10000
+#define MAXLENG	10000
+#define MAXSIZE 10001
 #define SERV_TCP_PORT 5051
 #include<stdio.h>
 #include<string.h>
@@ -10,7 +11,8 @@ void printenv();
 void err_dump(char*);
 void process_request(int);
 int readline(int, char*, int);
-
+void parser(char*, int);
+int checkargv(char*, char*, int);
 int main(int argc, char* argv[])
 {
 	int sockfd, newsockfd, clilen, childpid;
@@ -19,7 +21,7 @@ int main(int argc, char* argv[])
 	 *Open a TCP socket(an Internet stream socket).
 	 */
 	if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0){
-		err_dump("server:can't open stream socket");
+		err_dump("server:can't open stream socket\n");
 	}
 	//memset((char*)&serv_addr, 0, sizeof(serv_addr));
 	bzero((char *) &serv_addr, sizeof(serv_addr));
@@ -28,7 +30,7 @@ int main(int argc, char* argv[])
 	serv_addr.sin_port			= htons(SERV_TCP_PORT);
 
 	if(bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0){
-		err_dump("server:can't bind local address");
+		err_dump("server:can't bind local address\n");
 	}
 	listen(sockfd, 5);
 	while(1)
@@ -36,15 +38,14 @@ int main(int argc, char* argv[])
 		clilen = sizeof(cli_addr);
 		newsockfd = accept(sockfd, (struct sockaddr*)&cli_addr, &clilen);
 		if(newsockfd < 0){
-			err_dump("server:accept error");
+			err_dump("server:accept error\n");
 		}
 		if((childpid = fork()) < 0){
-			err_dump("server: fork error");
+			err_dump("server: fork error\n");
 		}else if(childpid == 0){/*child process*/
 			/*close original socket*/
 			close(sockfd);
 			/*process the request*/
-			//TODO process request
 			process_request(newsockfd);
 			exit(0);
 		}
@@ -56,25 +57,75 @@ void err_dump(char* msg){
 	exit(1);
 }
 void process_request(int sockfd){
-	int n;
-	char line[MAXLINE];
+	int n, rc, i;
+	char line[MAXSIZE];
+	//while(1)
+	//{
+	//	//The length of a single-line input will not exceed 10000 characters.
+	//	n = readline(sockfd, line, MAXLENG);
+	//	if(n == 0){
+	//		return;
+	//	}else if(n < 0){
+	//		err_dump("process_request:readline error\n");
+	//	}
+	//	//if(writen(sockfd, line, n)!= n){
+	//	//	err_dump("process_request:writen error");
+	//	//}
+	//}
 	while(1)
 	{
-		//The length of a single-line input will not exceed 10000 characters.
-		n = readline(sockfd, line, MAXLINE);
-		if(n == 0){
-			return;
-		}else if(n < 0){
-			err_dump("process_request:readline error");
+		rc = read(sockfd, line, MAXLENG);
+		if(rc < 0){
+			err_dump("readline:read error\n");
+		}else{//maybe contain not one command
+			for(i = 0 ; i < rc ; i++){
+				if( (line[i] == '\n' || line[i] == '\0') && i < (rc - 1) ){//contain more than one command
+					line[i] = '\0';
+					parser(line, i);
+					//TODO:not done
+				}
+			}
+			line[rc] = '\0';
+			parser(line, rc);
 		}
-		//if(writen(sockfd, line, n)!= n){
-		//	err_dump("process_request:writen error");
-		//}
 	}
+}
+void parser(char* line, int len){
+	char *buff;
+	char *argv[checkargv(line, " ", len)];
+	buff = strtok(line, " ");
+	while(buff != NULL)
+	{
+		buff = strtok(NULL, " ");
+	}
+	
+}
+int checkargv(char* str, char* delim, int len){
+	char tempstr[len + 1];
+	memset(tempstr, 0, sizeof(tempstr));
+	int i = 0;
+	for(i = 0 ; i < len ; i++){
+		tempstr[i] = str[i];
+	}
+	tempstr[i] = '\0';
+	char *buff;
+	int n = 0;
+	buff = strtok(tempstr, delim);
+	while(buff != NULL && buff[0] != '\n')
+	{
+		n++;
+		buff = strtok(NULL, delim);
+	}
+	return n;
 }
 int readline(int fd, char* ptr, int maxlen){
 	int n,rc;
-	read(fd, ptr, maxlen);
+	rc = read(fd, ptr, maxlen);
+	if(rc < 0){
+		err_dump("readline:read error\n");
+	}else if(rc < maxlen){
+		
+	}
 	
 }
 void mysetenv(char* name, char* value, int replace){
