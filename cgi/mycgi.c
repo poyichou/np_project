2493d8be0c91ef10f3edf6bdbefbe69db5713cf7
gammmmmble <bob2006tw@gmail.com>
2017-12-08 23:09:17 +0800

    cgi: understand spec rightly

--- cgi/mycgi.c
+++ cgi/mycgi.c
@@ -1,7 +1,8 @@
+//connect->read file->write to client->read response->read file->...
 #define F_CONNECTING 0
-#define F_READING 1
-#define F_WRITING 2
-#define F_DONE 3
+#define F_WRITING 1
+#define F_READING 2
+#define F_DONE 4
 #define MAXSIZE 256
 #include<stdio.h>
 #include<string.h>
@@ -23,7 +24,8 @@
 char host[5][MAXSIZE];
 int  port[5];
 char file[5][MAXSIZE];
-int sockfd[5];
+int  sockfd[5];
+FILE *fp[5];
 int connectTCP(char *addr, int port){
 	int sockfd;
 	struct sockaddr_in  serv_addr;
@@ -68,37 +70,48 @@ int connect_request(){
 	}
 	return connectnum;
 }
-int write_request(int sockfd, int idx, int *writen){
-	char req[strlen("GET / HTTP/1.1\r\nHost: \r\n\r\r") + strlen(host[idx]) + strlen(file[idx]) + 1];
-	snprintf(req, sizeof(req), "GET /%s HTTP/1.1\r\nHost: %s\r\n\r\r", file[idx], host[idx]);
-	int n = *writen;
-	n = write(sockfd, req + n, strlen(req) - n);
-	return n;
-}
-void send_request(int i, int *n, int *NeedWrite, int *status, fd_set *rs, fd_set *ws){
-	n[i] = write_request(sockfd[i], i, &n[i]);
-	NeedWrite[i] -= n[i];
-	//if not, write next time
-	if (n[i] <= 0 || NeedWrite[i] <= 0) {
-		// write finished
-		FD_CLR(sockfd[i], ws);
-		status[i] = F_READING;
-		FD_SET(sockfd[i], rs);
+void send_comm(int i, int *status, fd_set *rs, fd_set *ws){
+	char buff[MAXSIZE];
+	int wc = 0;
+	int writen = 0;
+	//get 1 line of commands
+	if(fgets(buff, MAXSIZE - 3, fp[i]) == NULL){
+		//finish reading file
+		return;
 	}
+	//change "\n" to "\r\n"
+	if(buff[strlen(buff)] == '\n')
+		buff[strlen(buff)] = '\0';
+	strcat(buff, "\r\n");
+	//send to server
+	while(writen < strlen(buff))
+	{
+		wc = write(sockfd[i], buff + writen, strlen(buff) - writen);
+		if(wc < 0){
+			perror("write command error");
+			exit(1);
+		}
+		writen += wc;
+	}
+	// write finished
+	FD_CLR(sockfd[i], ws);
+	status[i] = F_READING;
+	FD_SET(sockfd[i], rs);
 }
-int read_response(int sockfd, char *respmsg){
-	int rc = 0;
-	rc = read(sockfd, respmsg, MAXSIZE - 1);
-	return rc;
-}
-int recv_response(int i, int *n, int *status, int conn, char *respmsg, int sockfd, fd_set *rs){
-	n[i] = read_response(sockfd, respmsg);
-	if (n[i] <= 0) {
+int recv_response(int i, int *len, int *status, int conn, char *respmsg, int sockfd, fd_set *rs, fd_set *ws){
+	len[i] = read(sockfd, respmsg, MAXSIZE - 1);
+	if (len[i] <= 0) {
 		// read finished
 		FD_CLR(sockfd, rs);
 		status[i] = F_DONE ;
 		conn--;
+	}else if(len[i] < MAXSIZE - 1){
+		//read done
+		FD_CLR(sockfd, rs);
+		status[i] = F_WRITING;
+		FD_SET(sockfd, ws);
 	}
+	respmsg[len[i]] = '\0';
 	return conn;
 }
 void read_query(){
@@ -129,7 +142,7 @@ void print_result(char *respmsg, int len){
 	int writen = 0;
 	while(writen < len)
 	{
-		wc = write(1, respmsg + writen, strlen(respmsg) - writen);
+		wc = write(1, respmsg + writen, len - writen);
 		writen += wc;
 		if(wc < 0){
 			perror("write error");
@@ -137,7 +150,7 @@ void print_result(char *respmsg, int len){
 		}
 	}
 }
-void set_fd_set_status(fd_set *rfds, fd_set *wfds, fd_set *rs, fd_set *ws, int *n, int *status, int *NeedWrite, int conn){
+void set_fd_set_status(fd_set *rfds, fd_set *wfds, fd_set *rs, fd_set *ws, int *status, int conn){
 	//clear
 	FD_ZERO(rfds);
 	FD_ZERO(wfds);
@@ -147,22 +160,25 @@ void set_fd_set_status(fd_set *rfds, fd_set *wfds, fd_set *rs, fd_set *ws, int *
 	for(i = 0 ; i < conn ; i++){
 		FD_SET(sockfd[i], rs);
 		FD_SET(sockfd[i], ws);
-		n[i] = 0;
 		status[i] = F_CONNECTING;
-		NeedWrite[i] = strlen("GET / HTTP/1.1\r\nHost: \r\n\r\r") + strlen(host[i]) + strlen(file[i]) + 1;
+		fp[i] = fopen(file[i], "r");
+		if(fp[i] == NULL){
+			perror("fopen error");
+			exit(1);
+		}
 	}
 	*rfds = *rs; *wfds = *ws;
 }
-void fail_done_succeed_write(int i, int *status, fd_set *rs){
+void fail_done_succeed_write(int i, int *status, fd_set *ws){
 	int error = 0, errlen = sizeof(int);
 	if (getsockopt(sockfd[i], SOL_SOCKET, SO_ERROR, &error, (socklen_t*)&errlen) < 0 || error != 0) {
 		// non-blocking connect failed
 		perror("non blocking connect fail");
 		exit(1);
 	}
-	//the first thing to do if succeeded is write
-	status[i] = F_WRITING;
-	FD_CLR(sockfd[i], rs);
+	//the first thing to do if succeeded is read welcome
+	status[i] = F_READING;
+	FD_CLR(sockfd[i], ws);
 }
 int main()
 {
@@ -176,9 +192,9 @@ int main()
 	fd_set rs; /* active file descriptors*/
 	fd_set ws; /* active file descriptors*/
 	int nfds = FD_SETSIZE;
-	int n[5];//has writen or has read
-	int status[5], NeedWrite[5];
-	set_fd_set_status(&rfds, &wfds, &rs, &ws, n, status, NeedWrite, conn);
+	int len[5];//has writen or has read
+	int status[5];
+	set_fd_set_status(&rfds, &wfds, &rs, &ws, status, conn);
 	char respmsg[MAXSIZE];
 	while(conn > 0)
 	{
@@ -192,17 +208,17 @@ int main()
 		for(i = 0 ; i < conn ; i++){
 			//check connect succeeded or failed
 			if (status[i] == F_CONNECTING && (FD_ISSET(sockfd[i], &rfds) || FD_ISSET(sockfd[i], &wfds))){
-				fail_done_succeed_write(i, status, &rs);
+				fail_done_succeed_write(i, status, &ws);
 			}
-			//write first
+			//write
 			else if (status[i] == F_WRITING && FD_ISSET(sockfd[i], &wfds)){
-				send_request(i, n, NeedWrite, status, &rs, &ws);
+				send_comm(i, status, &rs, &ws);
 			}
-			//then read
+			//read
 			else if (status[i] == F_READING && FD_ISSET(sockfd[i], &rfds) ) {
-				conn = recv_response(i, n, status, conn, respmsg, sockfd[i], &rs);
+				conn = recv_response(i, len, status, conn, respmsg, sockfd[i], &rs, &ws);
 				//print all
-				print_result(respmsg, n[i]);
+				print_result(respmsg, len[i]);
 			}
 		}
 	}
